c81db25ef0296e7a829fd87de0b965e3831973fb
Colino <colcmorris@gmail.com>
2021-06-04 23:57:58 +0100

    Nearer to working version of console mortgage calc

--- src/main/java/com/mycompany/mortgage_int_calc_2021/Mortgage_interest.java
+++ src/main/java/com/mycompany/mortgage_int_calc_2021/Mortgage_interest.java
@@ -4,8 +4,8 @@
  * and open the template in the editor.
  */
 package com.mycompany.mortgage_int_calc_2021;
-import java.util.Date;
-import java.util.Calendar;
+import java.util.*;
+//import java.util.Calendar;
 import java.time.*;
 import java.time.Month;
 import java.time.Period;
@@ -27,24 +27,43 @@ public class Mortgage_interest
         // TODO code application logic here
         //System.out.println("hello world!");
         
+        
         Scanner line = new Scanner(System.in);
+        
         Finance_apr apr = new Finance_apr();
         
         Mortgage_interest mc = new Mortgage_interest();
+        //mc.debugDateCalendar(); // Comment out when not in use
+        
+        //mc.debugDateTime(); // Comment out when not in use
+        
+        // Get the values from console input.
         String monthly_repay = mc.getEnterMonthlyRepaymentPrompt(apr, line);
         String int_rate = mc.getEnterInterestRatePrompt(apr, line);
         String mort_remain = mc.getEnterMortgageRemainingPrompt(apr, line);
         
         //System.out.println(apr.promptForDateOfCalculations(false));
         String start_date= mc.getStartOrEndDatePrompt(apr, line, true);
+        
+        
+        if(start_date.trim().equals(""))
+        {
+           System.out.println("No start date entered. Will use a default date");
+        }
         String end_date= mc.getStartOrEndDatePrompt(apr, line, false);
         
+        if(end_date.trim().equals(""))
+        {
+           System.out.println("No end date entered. Will use a default date");
+        }        
         
 
-        //apr.setMonthRepayment(Integer.parseInt(monthly_repay));
+        // Set the values that were entered in the console
         apr.setMonthRepayment(Double.valueOf(monthly_repay));
         apr.setInterestRate(Double.valueOf(int_rate));
         apr.setMortgageRemaining(Double.valueOf(mort_remain));
+        apr.setDateToCalculateFrom(start_date);
+        apr.setDateToCalculateTo(end_date);
         
         //** Run the program
         apr.processMortgateInterestCalculation();
@@ -105,6 +124,7 @@ public class Mortgage_interest
         /* @todo
         Check that date is valid
         */
+            
         return start_or_end_date;
     }
     
@@ -115,5 +135,43 @@ public class Mortgage_interest
 //        apr.setMortgageRemaining(23233.82);
 //        apr.setInterestRate(1.64);
 //        apr.anotherHello();
-    }    
+    } 
+    
+    /**
+     * Testing Calender.set from input. Delete when not required.
+     */    
+    private void debugDateCalendar()
+    {
+        String debug_date = "2021-06-04";
+        //** spilt debug date into format accepted by Calendar.set method | e.g: Calendar.set(2022, 2,31)
+        
+        //Array date_set = new Array(debug_date.split("-").;
+        String date_set[] =debug_date.split("-");
+        System.out.println("== Debug date split ==");
+        System.out.println(Arrays.toString(date_set)); // Arrays is part of java.util.*
+        System.out.println("== For Calendar.set(): ==");
+        System.out.println(date_set[0] + "," + (Integer.valueOf(date_set[1]) -1) + "," + date_set[0]);
+        System.exit(0); // Exit with zero, as if this is normal!
+        
+    }
+    /**
+     * Testing LocalDate/Time. Delete when not required
+     */
+    private void debugDateTime()
+    {
+        //LocalTime current_time = LocalTime.now();
+        LocalDate current_date = LocalDate.now();
+        LocalDate future_date =  LocalDate.parse("2022-04-01");
+        //LocalDate future_date =  LocalDate.of(2021, 7, 14);
+
+        
+        //System.out.println("Current time: " + current_time.toString());
+        System.out.println("Current date: " + current_date.toString());
+        System.out.println("future date: " + future_date.toString());
+        double period = Duration.between(current_date.atStartOfDay(), future_date.atStartOfDay()).toDays();
+
+        System.out.println("Current date - Future date: " + period);
+        //System.out.println("Future time: " + future_time.toString());
+        System.exit(0);
+    }
 }
